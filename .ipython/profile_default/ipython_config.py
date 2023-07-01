1dc1b3722a67cb42ac8c619d3eb0bf92a25c4a15
Alexander Roberts <alexander.d.roberts@gmail.com>
2023-07-01 03:37:17 -0400

    Create ipython_config.py

---
+++ .ipython/profile_default/ipython_config.py
@@ -0,0 +1,22 @@
+# save to: ~/.ipython/profile_default/ipython_config.py
+from IPython.terminal.prompts import Prompts
+from IPython.terminal.prompts import Token
+
+
+class Prompt(Prompts):
+    def in_prompt_tokens(self, cli=None):
+        return [(Token.Prompt, '>>> ')]
+
+    def continuation_prompt_tokens(self, cli=None, width=None):
+        return [(Token.Prompt, '    ')]
+
+    def out_prompt_tokens(self, cli=None):
+        return [(Token.Prompt, '')]
+
+
+c.TerminalInteractiveShell.prompts_class = Prompt
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalIPythonApp.display_banner = False
+c.TerminalInteractiveShell.editing_mode = 'vi'
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
